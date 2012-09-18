89182f8d4bdf92dc9343d51c43773218e3855e8d
jeffknowles <jeffknowles@gmail.com>
2012-09-17 18:25:39 -0700

    made cosmetic and formatting changes

--- walkfunction.py
+++ walkfunction.py
@@ -1,217 +1,204 @@
 import numpy as np
 import scipy as sp
 
-#####################
-#### SUBFUNCTION ####
-#####################
 
+## sub functions
 def randpick(TRANSMAT):
-	choice = np.random.random_integers(1,1000,1)
-	pick = 0 if choice[0] <= TRANSMAT[0]*1000 else 1
-	return pick
+    choice = np.random.random_integers(1,1000,1)
+    pick = 0 if choice[0] <= TRANSMAT[0]*1000 else 1
+    return pick
 
 def orient_angle(x,y): ## relative to flow, away from flow = 180
-	if x >= 0 and y >= 0:
-		angle = np.arctan(x/y)*180/np.pi
-	if x >= 0 and y < 0:
-		angle = ((2*np.pi+np.arctan(x/y))*180/np.pi)-90
-	if x < 0 and y >= 0:
-		angle = (2*np.pi+np.arctan(x/y))*180/np.pi
-	if x < 0 and y < 0:
-		angle = (np.arctan(x/y)*180/np.pi)+90
-	return angle
+    if x >= 0 and y >= 0:
+        angle = np.arctan(x/y)*180/np.pi
+    if x >= 0 and y < 0:
+        angle = ((2*np.pi+np.arctan(x/y))*180/np.pi)-90
+    if x < 0 and y >= 0:
+        angle = (2*np.pi+np.arctan(x/y))*180/np.pi
+    if x < 0 and y < 0:
+        angle = (np.arctan(x/y)*180/np.pi)+90
+    return angle
 
-####################################
-########### MAIN FUNCTION ##########
-####################################
-def randwalk(SIMLENGTH,BINSIZE,FLOW_SPEED,IPOS,LATLINE,VERSION):
+def generate_current_plane(flow_version):
+    if flow_version.lower() == 'new':
+        current_plane = np.array([[ 0.08,  0.1 ,  0.1 ,  0.1 ,  0.1 ,  0.15],
+              [ 0.15 ,  0.3 ,  0.3 ,  0.5 ,  0.3 ,  0.15 ],
+              [ 0.15 ,  0.5 ,  0.4 ,  0.4 ,  0.5 ,  0.15 ],
+              [ 0.15 ,  1.0 ,  0.6 ,  0.6 ,  1.0 ,  0.15 ],
+              [ 0.15 ,  0.5 ,  1.0 ,  1.0 ,  0.5 ,  0.15 ],
+              [ 0.10 ,  0.1 ,  0.1 ,  0.1 ,  0.1 ,  0.10 ]])
+    elif flow_version.lower() == 'jeb':
+        current_plane = np.array([[ 0.05,  0.1 ,  0.1 ,  0.1 ,  0.1 ,  0.05],
+              [ 0.1 ,  0.4 ,  0.4 ,  0.4 ,  0.4 ,  0.1 ],
+              [ 0.1 ,  0.4 ,  0.5 ,  0.5 ,  0.4 ,  0.1 ],
+              [ 0.1 ,  0.4 ,  1.0 ,  0.8 ,  0.4 ,  0.1 ],
+              [ 0.1 ,  0.2 ,  0.2 ,  0.2 ,  0.2 ,  0.1 ],
+              [ 0.1 ,  0.1 ,  0.1 ,  0.1 ,  0.1 ,  0.1 ]])
+    return current_plane
 
-    ## set number of dimensions
-    NDIM = 3
 
-    ## define animal's size and drag (according to Liu et al 1997)
-    AREA = 2e-4   ## set the square area
-    Cdrag = 0.0287 ## from paper
-    MASS = 0.0004
+## main function
+def randwalk(simlength, binsize, flow_speed, Iposition, latline = True, flow_version = "JEB"):
+    # set number of dimensions
+    ndim = 3
 
-    ########################## Turning Parameters
-    TURNINGDIST = 'normal' ## select distribution from which to draw turns
-    LAMBDA = 1 ##set average length of time between turns
-    SDA_T = 0.1*np.pi ##standard deviation of turning angles for normal turning dist
-    SDA_P = 0.1*np.pi
-    AMT_T = np.pi  ##Angle of Maximum Turn for uniform turning distributon
-    AMT_P = np.pi
+    # define animal's size and drag (according to Liu et al 1997)
+    area = 2e-4   ## set the square area
+    cdrag = 0.0287 ## from paper
+    mass = 0.0004
 
-    THETA = 0 ## set initial orientation angles
-    PHI = 0
+    # Turning Parameters
+    turn_lambda = 1 # average length of time between turns
+    sda_t = 0.1*np.pi # standard deviation of turning angles for normal turning dist
+    sda_p = 0.1*np.pi
 
-    ########################## Set boundries
-    BOUNDTEST = 1        ## Turn boundries on or off (1 or 0)
-    MINS = np.array([0, 0, 0])       ## minimum boundries
-    MAXS = np.array([0.685, 0.15, 0.15])## maximum boundries
+    # initial orientation angles
+    theta = 0 
+    phi = 0
 
-    ########################## Set Gravity (deps on bouyancy)
-    GRAV = np.array([0, 0, -.05])
+    # Set boundries
+    bounded = True        ## Turn boundries on or off (True or False)
+    min_bounds = np.array([0, 0, 0])       ## minimum boundries
+    max_bounds = np.array([0.685, 0.15, 0.15])## maximum boundries
 
-    ########################## Set Flow
-    if FLOW_SPEED is 0:
-	FLOW_ON = 0
-    elif FLOW_SPEED > 0:
-	FLOW_ON = 1
-	
-	if VERSION == 'NEW':
-		CURRENT_X = np.array([[ 0.08,  0.1 ,  0.1 ,  0.1 ,  0.1 ,  0.15],
-		      [ 0.15 ,  0.3 ,  0.3 ,  0.5 ,  0.3 ,  0.15 ],
-		      [ 0.15 ,  0.5 ,  0.4 ,  0.4 ,  0.5 ,  0.15 ],
-		      [ 0.15 ,  1.0 ,  0.6 ,  0.6 ,  1.0 ,  0.15 ],
-		      [ 0.15 ,  0.5 ,  1.0 ,  1.0 ,  0.5 ,  0.15 ],
-		      [ 0.10 ,  0.1 ,  0.1 ,  0.1 ,  0.1 ,  0.10 ]])
-		CURRENT_X = CURRENT_X*FLOW_SPEED*0.01 # convert cm/s into m/s
-		
-	if  VERSION == 'JEB':
-		CURRENT_X = np.array([[ 0.05,  0.1 ,  0.1 ,  0.1 ,  0.1 ,  0.05],
-		      [ 0.1 ,  0.4 ,  0.4 ,  0.4 ,  0.4 ,  0.1 ],
-		      [ 0.1 ,  0.4 ,  0.5 ,  0.5 ,  0.4 ,  0.1 ],
-		      [ 0.1 ,  0.4 ,  1.0 ,  0.8 ,  0.4 ,  0.1 ],
-		      [ 0.1 ,  0.2 ,  0.2 ,  0.2 ,  0.2 ,  0.1 ],
-		      [ 0.1 ,  0.1 ,  0.1 ,  0.1 ,  0.1 ,  0.1 ]])
-		CURRENT_X = CURRENT_X*(FLOW_SPEED*0.01) # convert cm/s into m/s
-	
-    if FLOW_ON is 0:
-        CURRENTVEL = ([0,0,0])
-    if FLOW_ON is 1:
-        CURRENTVEL = ([CURRENT_X, 0, 0])
-    dCURRENTVEL = CURRENTVEL
-    
-    ## state Parameter
-    ## set power for each state
+    # Set Gravity (deps on bouyancy)
+    gravity = np.array([0, 0, -.05])
 
-    STATE_POWER = np.array([1e-12, 2e-4])
+    # Set Flow
+    if flow_speed == 0:
+        flow_on = False
+    elif flow_speed > 0:
+        flow_on = True
+        current_plane = generate_current_plane(flow_version)
+        current_plane = current_plane * flow_speed*0.01 # convert cm/s into m/s
 
-    ## define transition probability matrix for transitions among states
-    TRANSMAT = np.array([[0.991, 0.009], [0.05, 0.95]])
+    # set state parameters
+    state_power = np.array([1e-12, 2e-4]) # set power for each state
+    TRANSMAT = np.array([[0.991, 0.009],  # define transition probability matrix for transitions among states
+                         [0.05, 0.95]])
 
-    #### Preallocate
-    VEL = np.zeros((round(SIMLENGTH/BINSIZE),NDIM))
-    POS = np.zeros((round(SIMLENGTH/BINSIZE),NDIM))
-    ORIENT = np.zeros((round(SIMLENGTH/BINSIZE,1)))
-    POS[0,:] = IPOS
-    ###### Setup Counters and state record
-    PERIOD = 0
-    TURNCOUNT = 0
-    BINCOUNT = 0
-    state = 0
-    drag = 0.00075
-    ###############################
-    #### LAT LINE MODEL PARAMS ####
-    ###############################
-
+    ## set lateral line model params
     SWIM_LEN = 0 # starting value
     movecount = 0 # starting value
-    
-    TAUlat = 0.80 ## increasing slows spiking
-    latDOT = 0.0 ## starting value
-    latRESET = 0
-    latCUT = 1
-    k_ = 0.006 ## decreasing causes increased spiking
-    
+    TAUlat = 0.1 # increasing slows spiking (time constant of neuron)
+    latDOT = 0.0 # starting value (resting potential)
+    latRESET = 0 # reset potential
+    latCUT = 1 # spike threshold
+    k_ = 0.006 # decreasing causes increased spiking
 
-    ###################
-    #### MAIN LOOP ####
-    ###################
-    for ktime in range(0,int(SIMLENGTH/BINSIZE)-1):#(BINSIZE,SIMLENGTH,BINSIZE):
+    # Preallocate recording variables
+    time = np.arange(0,round(simlength/binsize)) * binsize
+    velocity = np.zeros((round(simlength/binsize),ndim))
+    position = np.zeros((round(simlength/binsize),ndim))
+    orientation = np.zeros((round(simlength/binsize,1)))
+    flow_rate = np.zeros((round(simlength/binsize,1)))
+    Vll = np.zeros((round(simlength/binsize,1)))
+    position[0,:] = Iposition
 
-        TURNCOUNT += 1 ## Count Bins since last turn
-        BINCOUNT += 1   ## COUNT BINS (absolute)
-     
-     ##  transition among states
+    # Setup counters and state record
+    period = 0
+    turncount = 0
+    bincount = 0
+    state = 0
+    drag = 0.00075
+
+    # MAIN LOOP 
+    for ktime in range(0,int(simlength/binsize)-1): #(binsize,simlength,binsize):
+        bincount += 1   ## COUNT BINS (absolute)
+        turncount += 1 ## Count Bins since last turn
+             
+        # transition among states
         state = randpick(TRANSMAT[state])
-        POWER= STATE_POWER[state]    
+        power = state_power[state]    
     
-    ## if it is time to select new orientation, generate turns
-        if TURNCOUNT > PERIOD:
-            ## Select Interval Before Next Turn (bins)
-	    PERIOD=round(np.random.exponential(LAMBDA,1)/BINSIZE)
-            ## Determine Theta and Phi Distributions
-            ## Select change in anlges
-	
-            if TURNINGDIST is 'normal':
-                dTHETA = np.random.normal(0,SDA_T,1) 
-                dPHI = np.random.normal(0,SDA_P,1)
-            elif TURNINGDIST is 'uniform':
-                dTHETA = np.random.uniform(-AMT_T, AMT_T,1)
-                dPHI = np.random.uniform(-AMT_P,AMT_P,1)
-	    
-	    ##Update Angles
-	    THETA += dTHETA
-            PHI += dPHI
+        # if it is time to select new orientation, generate turns
+        if turncount > period:
+            # Select Interval Before Next Turn (bins)
+            period=round(np.random.exponential(turn_lambda, 1) / binsize)
+            turncount = 0
+            # add change in anlges from normal distribution
+            theta += np.random.normal(0,sda_t,1) 
+            phi += np.random.normal(0,sda_p,1)
 
-	    TURNCOUNT = 0
-    
-    ##################
-    #### LAT LINE ####
-    ##################
+        # find current at the present position
+        if flow_on:
+            y_dot = int(position[bincount-1][1]*39.37)
+            z_dot = int(position[bincount-1][2]*39.37)
+            current_x = current_plane[y_dot][z_dot]
+            i_current_vel = [current_x, 0,0]
+        else:
+            current_x = 0
+            i_current_vel = [0, 0, 0]
+        flow_rate[bincount] = current_x
 
-	if FLOW_ON is 1:
-	    y_dot = int(POS[BINCOUNT-1][1]*39.37)
-            z_dot = int(POS[BINCOUNT-1][2]*39.37)
-	    CURRENT = CURRENT_X[y_dot][z_dot]
-	    dCURRENTVEL = ([CURRENT, 0,0])
-	elif FLOW_ON is 0:
-	    CURRENT = 0
-    
-        if LATLINE == 1:
-        
-	    latDOT += (CURRENT + k_*(latDOT))*BINSIZE/TAUlat
-
+        # lateral line model
+        if latline:
+            latDOT += (current_x + k_*(latDOT))*binsize/TAUlat
+            Vll[bincount] = latDOT
         if latDOT >= latCUT:
-	    latDOT = latRESET
-	    SWIM_LEN = 0.25 + np.random.random(1)*1.75
+            latDOT = latRESET
+            SWIM_LEN = 0.25 + np.random.random(1)*1.75
             movecount = 0
-                   
-    ##########################    
-    ###### Update FORCE ######
-    ##########################
-	
-    ##Calculate Thrust (dimensional)
-        if movecount < SWIM_LEN/BINSIZE:
-            THRUST = np.array([8e-5, np.sin(THETA)*np.sin(PHI)*POWER,
-			   np.cos(PHI)*POWER])
-            movecount +=1
-        elif movecount >= SWIM_LEN/BINSIZE:
-            THRUST = np.array([np.cos(THETA)*np.sin(PHI)*POWER,
-			   np.sin(THETA)*np.sin(PHI)*POWER, np.cos(PHI)*POWER])
-	    
-	ORIENT[BINCOUNT] = orient_angle(np.cos(THETA)*np.sin(PHI),np.sin(THETA)*np.sin(PHI))
-    ## Calculate Current
+                    
+        # Calculate Thrust (dimensional)
+        if movecount < SWIM_LEN/binsize: # if in swimming state, swimm towards flow
+            thrust = np.array([8e-5, 
+                            np.sin(theta)*np.sin(phi)*power,
+                            np.cos(phi)*power])
+            movecount += 1
+        elif movecount >= SWIM_LEN/binsize: # otherwise swim normally
+            thrust = np.array([np.cos(theta)*np.sin(phi)*power,
+                               np.sin(theta)*np.sin(phi)*power, 
+                               np.cos(phi)*power])
     
-    ##Calculate Drag
-    #S= np.linalg.norm(dCURRENTVEL - VEL[BINCOUNT-1,:]) ##find apparent curr. magnitude
-	if state is 1:
-		#Fdrag=((1/2.)*AREA*Cdrag*S**2) ## caluclate drag force magnitude
-	    Fdrag= drag*(dCURRENTVEL - VEL[BINCOUNT-1,:])#/S
-	    if any(np.isnan(Fdrag)):
-		Fdrag=0
-	elif state is 0:
-	    Fdrag = drag*(-VEL[BINCOUNT-1,:])
+        #Calculate Drag
+        #S= np.linalg.norm(dCURRENTvelocity - velocity[bincount-1,:]) ##find apparent curr. magnitude
+        if state is 1:
+            #Fdrag=((1/2.)*area*cdrag*S**2) ## caluclate drag force magnitude
+            Fdrag= drag*(i_current_vel - velocity[bincount-1,:])#/S
+            if any(np.isnan(Fdrag)):
+                Fdrag=0
+        elif state is 0:
+            Fdrag = drag*(-velocity[bincount-1,:])
 
-    ## caluclate force
-        F = (THRUST.flatten() + GRAV*MASS + Fdrag)
+        # caluclate force
+        F = (thrust.flatten() + gravity * mass + Fdrag)
+        # integrate    
+        velocity[bincount,:] = velocity[bincount-1] + F*binsize/mass # Update Velocity 
+        position[bincount,:] = position[bincount-1] + (velocity[bincount,:]*binsize) # Update Position
 
-    ###### Update Velocity ######
-	VEL[BINCOUNT,:] = VEL[BINCOUNT-1] + F*BINSIZE/MASS ## F*BINSIZE/MASS = acceleration
-    
-    ###### Update Position ######
-        POS[BINCOUNT,:] = POS[BINCOUNT-1] + (VEL[BINCOUNT,:]*BINSIZE)
+        # record orientation angle
+        orientation[bincount] = orient_angle(np.cos(theta)*np.sin(phi),np.sin(theta)*np.sin(phi))
 
-    
-    ###### If bounded mode is on, then confine position and velocity to bounds
-        if BOUNDTEST is 1:
-            for kdim in range(0,NDIM):
-                if (POS[BINCOUNT,kdim]<MINS[kdim] or POS[BINCOUNT,kdim]>MAXS[kdim]):
-                    VEL[BINCOUNT,kdim]=0
-		
-            POS[BINCOUNT,:]=np.maximum(MINS,POS[BINCOUNT,:])
-            POS[BINCOUNT,:]=np.minimum(MAXS,POS[BINCOUNT,:])
+        # If bounded mode is on, then confine position and velocity to bounds
+        if bounded:
+            for kdim in range(0, ndim):
+                if (position[bincount,kdim]<min_bounds[kdim] or position[bincount,kdim]>max_bounds[kdim]):
+                    velocity[bincount,kdim]=0
+            position[bincount,:]=np.maximum(min_bounds, position[bincount,:])
+            position[bincount,:]=np.minimum(max_bounds, position[bincount,:])
 
-    return POS,ORIENT
+    # pack output data
+    data = {'time': time,
+            'track': position,
+            'orientation': orientation,
+            'lateral_line': Vll,
+            'flow_rate': flow_rate}
+
+    return data
+
+
+if __name__ == "__main__":
+    latline = True
+    flow = 'JEB'
+    data = randwalk(300, 0.1, 5, np.array([0.37, 0.075, 0.15]), latline = latline, flow_version = flow)
+
+    import plot_functions
+    axes = plot_functions.plot_tank()
+    plot_functions.plot_track3d(data['track'], axes = axes)
+    plot_functions.plot_xdata(data)
+    plot_functions.plt.show()
+
+
+
+

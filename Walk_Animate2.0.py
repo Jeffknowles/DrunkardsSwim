addd0795594b1ad4b710a6608b5e86be12ec2e7d
Brian Schmidt <brian.patrick.schmidt@gmail.com>
2012-09-22 11:39:36 -0700

    Added data option and cosmetic changes

--- Walk_Animate2.0.py
+++ Walk_Animate2.0.py
@@ -11,23 +11,48 @@ import plot_functions
 
 track_data = {}
 FLOW_DYNAMICS = 'JEB'
+REAL_DATA = 1
 
-t_noflow = 10
-t_flow = 100
+t_noflow = 300
+t_flow = 300
 binsize = .1
-current = 2
+current = 8
 
-# generate all data w/ lateral line switched off
-LATLINE = False
-frog1 = wlk.randwalk(t_noflow, binsize, 0, np.array([0.37, 0.075, 0.15]), latline = LATLINE, flow_version = FLOW_DYNAMICS)
-frog1 = wlk.randwalk(t_flow, binsize, current, frog1,latline = LATLINE, flow_version = FLOW_DYNAMICS)
-frog1['track'] = frog1['track']*100
-
+if REAL_DATA == 1:
+    # Import No Flow data:
+    frog1_NF = np.genfromtxt('/Users/brianschmidt/Dropbox/TadpoleStuff/Tracked_data_CSV/NPF5_2_NF_lessFilt.csv',delimiter=',')
+    # downsample
+    index = frog1_NF[:,0]!=0
+    frog1_NF = frog1_NF[index]
+    frog1_NF = frog1_NF[0::3]
+    # reset the simulation length to reflect data
+    t_noflow = len(frog1_NF)
+    # Import Flow data:
+    frog1_F = np.genfromtxt('/Users/brianschmidt/Dropbox/TadpoleStuff/Tracked_data_CSV/NPF5_2_F_lessFilt.csv',delimiter=',')
+    index = frog1_F[:,0]!=0
+    frog1_F = frog1_F[index]
+    frog1_F = frog1_F[0::3]
+    # reset the simulation length to reflect data
+    t_flow = len(frog1_F)
+    # Append data into one structure:
+    frog1 = np.append(frog1_NF,frog1_F,axis=0)
+    frog1 = {'track': frog1,
+             'state': np.zeros((len(frog1)))}
+    FROG1NAME = 'Observed Data'
+else:   
+    # generate all data w/ lateral line switched off
+    LATLINE = False
+    frog1 = wlk.randwalk(t_noflow, binsize, 0, np.array([0.37, 0.075, 0.15]), latline = LATLINE, flow_version = FLOW_DYNAMICS)
+    frog1 = wlk.randwalk(t_flow, binsize, current, frog1,latline = LATLINE, flow_version = FLOW_DYNAMICS)
+    frog1['track'] = frog1['track']*100
+    FROG1NAME = 'NO Lateral Line Model'
+    
 # generate all data w/ lateral line switched on
 LATLINE = True
-frog2 = wlk.randwalk(t_noflow, binsize, 0, np.array([0.37, 0.075, 0.15]), latline = LATLINE, flow_version = FLOW_DYNAMICS)
+frog2 = wlk.randwalk(t_noflow, binsize, 0, np.array([0.34, 0.075, 0.15]), latline = LATLINE, flow_version = FLOW_DYNAMICS)
 frog2 = wlk.randwalk(t_flow, binsize, current, frog2,latline = LATLINE, flow_version = FLOW_DYNAMICS)
 frog2['track'] = frog2['track']*100
+frog2['track'] = frog2['track'][0:len(frog1['track'])]
 
 flow = np.ones(frog1['track'].shape[0],'int') * current
 flow[0: (float(t_noflow)/ binsize)] = 0
@@ -57,21 +82,23 @@ class SubplotAnimation(animation.TimedAnimation):
         ax1.pbaspect = [1.4, 0.4, 0.4]
         ax2.pbaspect = [1.4, 0.4, 0.4]
 
-        self.flowtexta1 = ax1.text2D(0.04, 0.65, 'Flow 0 cm/s', fontsize=18, transform=ax1.transAxes)
-        self.flowtexta2 = ax2.text2D(0.04, 0.65, 'Flow 0 cm/s', fontsize=18, transform=ax2.transAxes)
-        self.statetexta1 = ax1.text2D(0.04, 0.85, 'State 0', fontsize=18, transform=ax1.transAxes)
-        self.statetexta2 = ax2.text2D(0.04, 0.85, 'State 0', fontsize=18, transform=ax2.transAxes)
+        self.flowtexta1 = ax1.text2D(0.001, 0.65, 'Flow 0 cm/s', fontsize=18, transform=ax1.transAxes)
+        self.flowtexta2 = ax2.text2D(0.001, 0.65, 'Flow 0 cm/s', fontsize=18, transform=ax2.transAxes)
+        if REAL_DATA == 0:
+            self.statetexta1 = ax1.text2D(0.001, 0.85, 'State 0', fontsize=18, transform=ax1.transAxes)
+        self.statetexta2 = ax2.text2D(0.001, 0.85, 'State 0', fontsize=18, transform=ax2.transAxes)
+        self.framerate = ax1.text2d(0.90, 0.85, '3x', fontsize = 18, transform=ax1.transAxes)
 
         plot_functions.plot_tank(ax1)
         plot_functions.plot_tank(ax2)
 
-        ax1.set_xlabel('x')
-        ax1.set_ylabel('y')
-        ax1.set_zlabel('z')
+        #ax1.set_xlabel('x')
+        #ax1.set_ylabel('y')
+        #ax1.set_zlabel('z')
 
-        ax2.set_xlabel('x')
-        ax2.set_ylabel('y')
-        ax2.set_zlabel('z')
+        #ax2.set_xlabel('x')
+        #ax2.set_ylabel('y')
+        #ax2.set_zlabel('z')
         
         self.line1 = Line3D([], [], [], color='black')
         self.line1a = Line3D([], [], [], color='red', linewidth=2)
@@ -93,7 +120,7 @@ class SubplotAnimation(animation.TimedAnimation):
         ax1.set_xlim(0, 68)
         ax1.set_ylim(0, 15)
         ax1.set_zlim(0, 15)
-        ax1.set_title('NO Lateral Line Model')
+        ax1.set_title(FROG1NAME)
         ax1.set_aspect('equal')
 
         ax2.set_xlim(0, 68)
@@ -109,7 +136,9 @@ class SubplotAnimation(animation.TimedAnimation):
         ax2.set_xticks([0, 68])
         ax2.set_yticks([])
         ax2.set_zticks([0, 15])
-       
+
+        plt.rc('xtick', labelsize=18)
+        
         animation.TimedAnimation.__init__(self, fig, interval=10, blit=False)
 
     def _draw_frame(self, framedata):
@@ -134,8 +163,10 @@ class SubplotAnimation(animation.TimedAnimation):
 
         self.flowtexta1.set_text('flow ' + str(self.flow[head]) + ' cm/s')
         self.flowtexta2.set_text('flow ' + str(self.flow[head]) + ' cm/s')
-        self.statetexta1.set_text('state ' + str(self.state1[head]))
+        if REAL_DATA == 0:
+            self.statetexta1.set_text('state ' + str(self.state1[head]))
         self.statetexta2.set_text('state ' + str(self.state2[head]))
+        
 
         self._drawn_artists = [self.line1, self.line1a, self.line1e,
                                self.line2, self.line2a, self.line2e]
